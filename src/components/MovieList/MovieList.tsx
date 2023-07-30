1d603ddc7fcb7f2f03c61e423a6691d95bbb2e61
tanveer-98 <tnvrahmed98@gmail.com>
2023-07-30 14:25:45 +0530

    feat : added button component

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,6 +1,13 @@
-import React, { useState, useEffect, useRef, useContext, useMemo , useCallback } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useContext,
+  useMemo,
+  useCallback,
+} from "react";
 import { MovieContext } from "../../context/MovieContext";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import "./movielistStyles.css";
 import { css } from "@emotion/css";
 import MovieCard from "../MovieCard/MovieCard";
@@ -8,7 +15,7 @@ import Button from "../Button/Button";
 
 const moviecontainer = css`
   display: grid;
-  margin-top : 200px;
+  margin-top: 200px;
   justify-content: center;
   align-items: center;
   width: 100%;
@@ -22,72 +29,76 @@ const moviecontainer = css`
   }
 `;
 const navContainer = css`
-display:flex;
-justify-content:center;
-align-items:center;`
- 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const headingStyle = css`
- margin:0px;
- flex : 1;
- color :#c5c5c5;
-`
+  margin: 0px;
+  flex: 1;
+  color: #c5c5c5;
+`;
 
 const buttonStyle = css`
-  color :white ;
-  background-color : #4f4f4f;
-  box-shadow : 2px 2px 10px rgba(0,0,0,1);
+  color: white;
+  background-color: #4f4f4f;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 1);
   padding: 4px 10px;
-  border-radius : 5px;
-  border : none;
-  cursor:pointer;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
 
-  &:hover{
-    box-shadow : 2px 2px 10px rgba(0,0,0,0.5);
+  &:hover {
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
   }
-`
+`;
 const ScrollContainer = css`
   width: 100%;
   height: 200px;
-  display : flex;
-  justify-content : center;
-  align-items : center;
-
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const MovieList = () => {
-  
   const movies = useContext(MovieContext);
   const [visibleMovies, setVisibleMovies] = useState([]);
 
   const navigate = useNavigate();
   const listRef = useRef<HTMLDivElement>(null);
 
-  const handleNavigate = ()=>{
+  const handleNavigate = () => {
     setVisibleMovies([]);
-    setTimeout(()=>{
-      navigate('/favmovies')
-    },100)
-  }
+    setTimeout(() => {
+      navigate("/favmovies");
+    }, 100);
+  };
 
   const loadMoreMovies = useCallback(() => {
-
     const nextMovies = movies?.slice(
       visibleMovies.length,
       visibleMovies.length + 5
     );
- 
-    setVisibleMovies((prev)=>{
-      if(prev!= null && JSON.stringify(prev[0]) != JSON.stringify(nextMovies[0]))
-      return [...prev, ...nextMovies]
-      else return [...prev]
+
+    setVisibleMovies((prev) => {
+      if (
+        prev != null &&
+        JSON.stringify(prev[0]) != JSON.stringify(nextMovies[0])
+      )
+        return [...prev, ...nextMovies];
+      else return [...prev];
     });
-  },[visibleMovies.length]);
-  
-  const handleIntersection = useCallback((entries : IntersectionObserverEntry[]) => {
-    if (entries[0].isIntersecting) {
-      loadMoreMovies();
-    }
-  },[loadMoreMovies]);
+  }, [visibleMovies.length]);
+
+  const handleIntersection = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting) {
+        loadMoreMovies();
+      }
+    },
+    [loadMoreMovies]
+  );
 
   useEffect(() => {
     const observer = new IntersectionObserver(handleIntersection, {
@@ -107,10 +118,8 @@ const MovieList = () => {
   }, [handleIntersection]);
 
   useEffect(() => {
-
     setVisibleMovies(movies.slice(0, 5));
   }, []);
-
 
   return (
     <div>
@@ -123,45 +132,39 @@ const MovieList = () => {
           zIndex: 10,
           backgroundColor: "gray",
           padding: "20px 0px",
-          margin : "0px",
+          margin: "0px",
           top: "0px",
           left: "0px",
         })}
       >
-        <h3 className = {headingStyle}>MOVIES TO WATCH</h3>
-        <button className={buttonStyle} onClick = {handleNavigate}>
-        My Favourites
+        <h3 className={headingStyle}>MOVIES TO WATCH</h3>
+        <button className={buttonStyle} onClick={handleNavigate}>
+          My Favourites
         </button>
-
-
       </div>
- 
+
       <div className={moviecontainer}>
-        {
-          visibleMovies.map((movie: any) => {
-            return (
-              <MovieCard
-                Poster={movie.Poster}
-                key={movie.id}
-                id={movie.id}
-                Title={movie.Title}
-                Genre={movie.Genre}
-                Year={movie.Year}
-                Type={movie.Type}
-              />
-             
-            );
-          })
-      
-        }
+        {visibleMovies.map((movie: any) => {
+          return (
+            <MovieCard
+              Poster={movie.Poster}
+              key={movie.id}
+              id={movie.id}
+              Title={movie.Title}
+              Genre={movie.Genre}
+              Year={movie.Year}
+              Type={movie.Type}
+            />
+          );
+        })}
 
-<div className={ScrollContainer}>
-           <Button bgcolor ="#33ff" text="Scroll for More">
-            Scroll For More 
-           </Button>
-            </div>
-      <div ref={listRef} />
+        <div className={ScrollContainer}>
+          <Button bgcolor="#33ff" text="Scroll for More">
+            Scroll For More
+          </Button>
+        </div>
       </div>
+      <div ref={listRef} />
     </div>
   );
 };
